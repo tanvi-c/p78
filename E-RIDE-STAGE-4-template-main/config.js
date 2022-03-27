46ccfa03ef3f7d1605c4428cc4c5b7593351b59b
tanvi-c <87154518+tanvi-c@users.noreply.github.com>
2022-03-27 19:02:22 -0400

    Add files via upload

---
+++ E-RIDE-STAGE-4-template-main/config.js
@@ -0,0 +1,19 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+//Paste your firebaseConfig here
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBEDXLJe0QGmBTh28WoclR16HKgXMEwAfY",
+    authDomain: "e-ride-d922d.firebaseapp.com",
+    projectId: "e-ride-d922d",
+    storageBucket: "e-ride-d922d.appspot.com",
+    messagingSenderId: "866073168065",
+    appId: "1:866073168065:web:e28fb1b52c4b92ee036c40"
+  };
+  
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
+
+
